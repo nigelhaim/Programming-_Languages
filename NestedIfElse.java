110d66df9a4743bae7cbd9f2e82ecda7b7059c7c
nigelhaim <nigelsebastian21@gmail.com>
2023-10-04 22:54:42 +0800

    Working input of .txt file lines of codes

--- NestedIfElse.java
+++ NestedIfElse.java
@@ -1,46 +1,75 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class NestedIfElse {
     /*
      * This is a program that checks the syntax of the nested if else 
      * statement. 
-     * TODO: Input a multi line code and print
+     * TODO: 
+     * 
+     * COMPLETED: Input a multi line code and print
      */
     public static void main(String[] args) {
         System.out.print("==============\nBeginTest\n==============");
         System.out.println();
         List<String> tokens = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please input your code:");
-        while (scanner.hasNext()) {
-            tokens.add(scanner.next());
-        }
-        System.out.println("===============");
-        System.out.println(tokens);
-        scanner.close();
-        System.out.println(tokens);
-    }
+        HashMap<Integer, String> token = new HashMap<Integer, String>();
+        try{
+            File file=new File("./IncorrectInput.txt");    //creates a new file instance  
+            FileReader fr=new FileReader(file);   //reads the file  
+            BufferedReader br=new BufferedReader(fr);  
+            System.out.println("Importing code");
+            int curlyCount = 0;
+            int pharCount = 0; 
+            int LineCount = 0;
+            String inp;
+            while ((inp=br.readLine())!=null) {
+                //System.out.println("Inserting " + inp);
+                if(inp.contains("{")){
+                    //System.out.println("curlyCount++");
+                    curlyCount++;
+                }
+                if(inp.contains("}")){
+                    //System.out.println("curlyCount--");
+                    curlyCount--;
+                }
 
-    public void checkif(String s){
+                if(inp.contains("(")){
+                    //System.out.println("PharCount++");
+                    pharCount++;
+                }
+                if(inp.contains(")")){
+                    //System.out.println("PharCount--");
+                    pharCount--;
+                }
+                //System.out.println("Curly braces count: " + curlyCount + "\nPharanthesis Count: " + pharCount); 
+                tokens.add(inp);
+                token.put(LineCount,inp);
+                LineCount++;
+                if((curlyCount != 0 || pharCount != 0) || !(inp.contains(")") || inp.contains("}"))){
+                    //System.out.print("Added " + inp);
+                    continue;
+                }
+                else{
+                    break;
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+       
+        System.out.println("===============");
+        System.out.println("\nPrinting tokens");
+        System.out.println("Token list: " + tokens);
+        System.out.println();
+        int lineNumber = 0;
+        // for(String t : tokens){
+        //     System.out.println(lineNumber + " | " + t);
+        //     lineNumber++;
+        // }
+
+        for(Map.Entry<Integer, String> set: token.entrySet()){
+            System.out.println(set.getKey() + " || " + set.getValue());
+        }
     }
 }
 
-/*
- Test input 
-    int x = 1; 
-    if(x+1 == 2){
-     System.out.print("True");
-     if(x+2 == 3){
-         System.out.print("True");
-     }else{
-         System.out.print("False");
-     }
-    }else{
-     System.out.print("False");
-    }
-
- */
-
- 
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Programming-_Languages
+
+## Specifications 
+
+java version "21" 2023-09-19 LTS
+Java(TM) SE Runtime Environment (build 21+35-LTS-2513)
+Java HotSpot(TM) 64-Bit Server VM (build 21+35-LTS-2513, mixed mode, sharing)
+
+# Message 
+- The main program for the project is NestedIfElse.java 
+- Recommended to use VSC for smooth operation
+
+## Main Program 
+- You can use List and HashMap please comment out depending on your liking
+#### InputBasic.txt
+- This contains less complicated line which contains a valid Nested If-then-Else statement
+
+#### Input.txt
+- This contains a bit more complicaed line which detects variable initiations, printing, etc. 
+
+#### IncorrectInput.txt
+- This contains an incorrect Nested If-then-Else statement
+
+#TODO
+- Make if statement syntax checker
+- Make then statement syntax checker 
+- Make else statment syntax checker 
+- figure out how to check every if else 
+- make all syntax checker into functions 
+#COMPLETED
+- Successful input of multiple lines of a .txt file 
+
+## Side notes 
+- My idea on the project is that every if statement will enter a recursion method to check the current If statement's condition. If it returned true then it will ender inside then checks if theres another if statement. If there is antoher if statement then it will return to an another recurssion method then checks the syntaxes (this is my reason why we put the syntax checkers in functions). When everything is good then we end the program with a positive prompt. Else we end the program by printing what line is the error and the comment why.
