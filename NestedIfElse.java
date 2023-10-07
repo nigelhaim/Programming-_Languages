0a6e56bc21e7e859a5a7d9999fa4ec8a4d05d8ab
nigelhaim <nigelsebastian21@gmail.com>
2023-10-07 13:50:30 +0800

    Push for ready to pull

--- NestedIfElse.java
+++ NestedIfElse.java
@@ -4,17 +4,16 @@ public class NestedIfElse {
     /*
      * This is a program that checks the syntax of the nested if else 
      * statement. 
-     * TODO: 
-     * 
-     * COMPLETED: Input a multi line code and print
      */
     public static void main(String[] args) {
         System.out.print("==============\nBeginTest\n==============");
         System.out.println();
-        List<String> tokens = new ArrayList<>();
+        //List<String> tokens = new ArrayList<>();
         HashMap<Integer, String> token = new HashMap<Integer, String>();
+        int ifCount = 0;
+        int elseCount = 0;
         try{
-            File file=new File("./IncorrectInput.txt");    //creates a new file instance  
+            File file=new File("./InputBasic.txt");    //creates a new file instance  
             FileReader fr=new FileReader(file);   //reads the file  
             BufferedReader br=new BufferedReader(fr);  
             System.out.println("Importing code");
@@ -41,8 +40,13 @@ public class NestedIfElse {
                     //System.out.println("PharCount--");
                     pharCount--;
                 }
+                if(inp.toUpperCase().contains("IF")){
+                    ifCount++;
+                }else if(inp.toUpperCase().contains("ELSE")){
+                    elseCount++;
+                }
                 //System.out.println("Curly braces count: " + curlyCount + "\nPharanthesis Count: " + pharCount); 
-                tokens.add(inp);
+                //tokens.add(inp);
                 token.put(LineCount,inp);
                 LineCount++;
                 if((curlyCount != 0 || pharCount != 0) || !(inp.contains(")") || inp.contains("}"))){
@@ -59,9 +63,16 @@ public class NestedIfElse {
        
         System.out.println("===============");
         System.out.println("\nPrinting tokens");
-        System.out.println("Token list: " + tokens);
+        //System.out.println("Token list: " + tokens);
+        //System.out.println("Token list: " + tokens);
         System.out.println();
-        int lineNumber = 0;
+        System.out.println("===============");
+        System.out.println("Count");
+        System.out.println("Number of if statements: " + ifCount);
+        System.out.println("Number of else statements: " + elseCount);
+        System.out.println("===============");
+        System.out.println();
+        //int lineNumber = 0;
         // for(String t : tokens){
         //     System.out.println(lineNumber + " | " + t);
         //     lineNumber++;
@@ -73,3 +84,4 @@ public class NestedIfElse {
     }
 }
 
+    
