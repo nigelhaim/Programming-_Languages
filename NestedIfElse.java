e7711c7f9851cc6384678c094a2f99614c24d61e
nigelhaim <nigelsebastian21@gmail.com>
2023-10-14 00:26:51 +0800

    Almost working recurssion

--- NestedIfElse.java
+++ NestedIfElse.java
@@ -13,7 +13,7 @@ public class NestedIfElse {
         int ifCount = 0;
         int elseCount = 0;
         try{
-            File file=new File("./InputBasic.txt");    //creates a new file instance  
+            File file=new File("./InputAdvanced.txt");    //creates a new file instance  
             FileReader fr=new FileReader(file);   //reads the file  
             BufferedReader br=new BufferedReader(fr);  
             System.out.println("Importing code");
@@ -47,14 +47,9 @@ public class NestedIfElse {
                 }
                 //System.out.println("Curly braces count: " + curlyCount + "\nPharanthesis Count: " + pharCount); 
                 //tokens.add(inp);
-                token.put(LineCount,inp);
-                LineCount++;
-                if((curlyCount != 0 || pharCount != 0) || !(inp.contains(")") || inp.contains("}"))){
-                    //System.out.print("Added " + inp);
-                    continue;
-                }
-                else{
-                    break;
+                if(!inp.equals("")){
+                    token.put(LineCount,inp);
+                    LineCount++;
                 }
             }
         }catch(Exception e){
@@ -77,10 +72,63 @@ public class NestedIfElse {
         //     System.out.println(lineNumber + " | " + t);
         //     lineNumber++;
         // }
+        
+        scanIF(token, false);
+    }
+    static void scanIF(HashMap<Integer, String> token, boolean skipIF){
+        HashMap<Integer, String> ifBlock = new HashMap<Integer, String>();
+        int curlyCount = 0;
+        int pharCount = 0; 
+        boolean addBlock = false;
+        for(Map.Entry<Integer, String> met: token.entrySet()){
+            String s = met.getValue();
+            int lineNum = met.getKey();
+            // System.out.println(met.getKey() + " || " + met.getValue());  
+            if(skipIF == false){
+                if(s.toUpperCase().contains("IF")){
+                    addBlock = true;
+                }  
+            }
+            else{
+                skipIF = false;
+                curlyCount--;
+            }
+            if(s.contains("{")){
+                //System.out.println("curlyCount++");
+                curlyCount++;
+            }
+            if(s.contains("}")){
+                //System.out.println("curlyCount--");
+                curlyCount--;
+            }
 
-        for(Map.Entry<Integer, String> set: token.entrySet()){
-            System.out.println(set.getKey() + " || " + set.getValue());
+            if(s.contains("(")){
+                //System.out.println("PharCount++");
+                pharCount++;
+            }
+            if(s.contains(")")){
+                //System.out.println("PharCount--");
+                pharCount--;
+            }
+            
+            if(addBlock){
+                ifBlock.put(lineNum, s);
+            }
+
+            if(curlyCount == 0 && pharCount == 0){
+                addBlock = false;
+            }  
         }
+        System.out.println();
+        for(Map.Entry<Integer, String> get :ifBlock.entrySet()){
+            System.out.println(get.getKey() + " // " + get.getValue());
+        }
+        System.out.println("-------------------------------------------------------");
+        skipIF = true;
+        if(!ifBlock.isEmpty()){
+            scanIF(ifBlock, skipIF);
+        }
+        return;
     }
 }
 
